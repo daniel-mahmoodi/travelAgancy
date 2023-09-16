b9d928af7a2ed0c457065794d438a3739d0e4477
daniel <mahmoodidanial2@gmail.com>
2023-09-16 20:48:13 +0330

    feat: add jalaly moment and add hook to use it

---
+++ src/components/Hooks/useDataConverter.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import moment from "jalali-moment";
+
+const useDateConverter = (date) => {
+  const [convertedDate, setConvertedDate] = useState({
+    Year: "",
+    dayOfWeek: "",
+    DayOfMonth: "",
+    TimeOfDay: "",
+    hour: "",
+    Month: "",
+  });
+  useEffect(() => {
+    if (date) {
+      const gmtDateTime = moment.utc(date, "YYYY-MM-DD HH:mm:ss");
+      const iranDateTime = gmtDateTime.local().locale("fa");
+      const iranDateTimeFormatted = iranDateTime.format(
+        "jMM/jDD/jYYYY HH:mm:ss"
+      );
+      const Year = moment(iranDateTimeFormatted, "jMM/jDD/jYYYY HH:mm:ss")
+        .locale("fa")
+        .format("YYYY");
+      const dayOfWeek = moment(iranDateTimeFormatted, "jMM/jDD/jYYYY HH:mm:ss")
+        .locale("fa")
+        .format("dddd");
+      const Month = moment(iranDateTimeFormatted, "jMM/jDD/jYYYY HH:mm:ss")
+        .locale("fa")
+        .format("MMMM");
+      const TimeOfDay = moment(iranDateTimeFormatted, "jMM/jDD/jYYYY HH:mm:ss")
+        .locale("fa")
+        .format("HH:mm:ss");
+      const hour = moment(iranDateTimeFormatted, "jMM/jDD/jYYYY HH:mm:ss")
+        .locale("fa")
+        .format("HH");
+      const DayOfMonth = moment(iranDateTimeFormatted, "jMM/jDD/jYYYY HH:mm:ss")
+        .locale("fa")
+        .format("D");
+
+      setConvertedDate({
+        Year,
+        dayOfWeek,
+        DayOfMonth,
+        TimeOfDay,
+        hour,
+        Month,
+      });
+    }
+  }, [date]);
+
+  return convertedDate;
+};
+
+export default useDateConverter;
